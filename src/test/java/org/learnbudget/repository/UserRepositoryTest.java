51713be80c9e606f740e334146c40c1c07cdce87
Chimaje Agada <94375715+chimaje@users.noreply.github.com>
2026-08-11 22:41:51 +0100

    updating testcase

--- src/test/java/org/learnbudget/repository/UserRepositoryTest.java
+++ src/test/java/org/learnbudget/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
 import org.learnbudget.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -20,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @DisplayName("User Repository Tests")
 class UserRepositoryTest {
 
